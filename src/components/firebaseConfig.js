e7a45ce03ba5bf92b4b18041b6570b30c2b2a9e7
Ismail <ismail.kerja95@gmail.com>
2018-01-23 07:13:26 +0700

    completed

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDWpgrDqeaVLT2cduRBOUsKPm0tNTZCqrE",
+    authDomain: "vuefs-prod-15d82.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-15d82.firebaseio.com",
+    projectId: "vuefs-prod-15d82",
+    storageBucket: "vuefs-prod-15d82.appspot.com",
+    messagingSenderId: "827407573168"
+}
